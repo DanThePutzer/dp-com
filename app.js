74d990c35c7fb3b942c0056607e9e40c655a7c75
Daniel Putzer <putzer.daniel@rolmail.net>
2018-10-20 23:19:43 +0200

    First Setup

---
+++ app.js
@@ -0,0 +1,15 @@
+
+// Setting up Express
+const express = require('express')
+const app = express();
+const path = require('path');
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.use("/styles", express.static(__dirname + '/styles'));
+
+app.listen(3000, () => {
+  console.log('Server online at Port 3000');
+});
