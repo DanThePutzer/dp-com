2eb6171dd4d85f72884eb16a7f96765a604af6b0
Daniel Putzer <putzer.daniel@rolmail.net>
2018-10-25 17:58:14 +0200

    Major Progress with basic structure

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ const express = require('express')
 const app = express();
 const path = require('path');
 
-app.use(express.static('style'));
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.listen(3000, () => {
-  console.log('Server online at Port 3000');
+app.listen(1999, () => {
+  console.log('Server online at Port 1999');
 });
