285e3881e13b21409209b8b693a4882279371286
Daniel Putzer <putzer.daniel@rolmail.net>
2019-02-05 18:10:55 +0100

    Structure Improvement

--- dist/app.c328ef1a.js
+++ dist/app.c328ef1a.js
@@ -21454,7 +21454,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58313" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64206" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
